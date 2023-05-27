6963e80e37965c7c8a7471b4aaa7dabfb514993d
Abhishek Poddar <67195536+abhi8054@users.noreply.github.com>
2023-05-27 16:12:20 +0530

    dockerise is done

--- server/config/config.js
+++ server/config/config.js
@@ -1,16 +1,10 @@
 const firebaseConfig = Object.freeze({
-  // apiKey: process.env.API_KEY,
-  // authDomain: process.env.AUTH_DOMAIN,
-  // projectId: process.env.PROJECT_ID,
-  // storageBucket: process.env.STORAGE_BUCKET,
-  // messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  // appId: process.env.APP_ID
-  apiKey: "AIzaSyDpYsUEvQMFhWpTyVF998SyWAGI-sRqIrg",
-  authDomain: "freelance-450d2.firebaseapp.com",
-  projectId: "freelance-450d2",
-  storageBucket: "freelance-450d2.appspot.com",
-  messagingSenderId: "666022868589",
-  appId: "1:666022868589:web:fd1dfdfe716f0a1c96f164"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 });
 
 module.exports = firebaseConfig
